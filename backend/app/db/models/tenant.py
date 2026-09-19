f79c0e4ecc213dc912a105a4ee109391f0576a6d
gopinathv19 <gopinath5166296@gmail.com>
2026-09-19 22:41:24 +0530

    feat: Very small change tenant.user_owner_id fk user.id is added in the table

--- backend/app/db/models/tenant.py
+++ backend/app/db/models/tenant.py
@@ -2,10 +2,11 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy import UUID,ForeignKey
 from app.db.database import Base
 from app.db.models.mixins import StatusColumn, TimestampCreated, UUIDPrimaryKey
 
+import uuid
 if TYPE_CHECKING:
     from app.db.models.application import Application
 
@@ -16,7 +17,7 @@ class Tenant(UUIDPrimaryKey, StatusColumn, TimestampCreated, Base):
     __tablename__ = "tenants"
 
     name: Mapped[str] = mapped_column(String(255), nullable=False)
-
+    user_owner_id:Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True),ForeignKey("users.id",ondelete="CASCADE"),nullable=False)
     applications: Mapped[list["Application"]] = relationship(
         back_populates="tenant",
         cascade="all, delete-orphan",
